69b6bc58121a45796276d0a5633f9ab2a3c45fdc
Ori Suchy <orisuchy@gmail.com>
2021-11-11 16:51:48 +0200

    fixed pdf functions

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.tools.PDFText2HTML;
 import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 
@@ -24,9 +25,7 @@ import javax.xml.parsers.ParserConfigurationException;
 public class Test {
 
     public static void toImage(URL url) throws IOException {
-        File file = new File(url.getFile());
         InputStream is = url.openStream();
-//        byte[] array = FileUtils.readFileToByteArray(file);
 
         PDDocument document = PDDocument.load(is);
         PDFRenderer pdfRenderer = new PDFRenderer(document);
@@ -37,9 +36,10 @@ public class Test {
             BufferedImage bim = pdfRenderer.renderImageWithDPI(pageCounter, 300, ImageType.RGB);
 
             // suffix in filename will be used as the file format
-            ImageIOUtil.writeImage(bim, "./output/images/pdfImage" + "-" + (pageCounter++) + ".png", 300);
+            ImageIOUtil.writeImage(bim, "./output/Images/pdfImage" + "-" + (++pageCounter) + ".png", 300);
         }
         document.close();
+        is.close();
     }
 
     public static void toHTML(URL url) throws IOException, ParserConfigurationException {
@@ -47,40 +47,42 @@ public class Test {
         PDDocument pdf = PDDocument.load(is);
         PDDocument page = new PDDocument();
         page.addPage(pdf.getPage(3));
- //        PDDocument document = PDDocument.load(is);
-//        PDFRenderer pdfRenderer = new PDFRenderer(document);
-//        int pageCounter = 0;
-//        for (PDPage page : document.getPages())
-//        {
-//            // note that the page number parameter is zero based
-//            BufferedImage bim = pdfRenderer.renderImageWithDPI(pageCounter, 300, ImageType.RGB);
-//
-//            // suffix in filename will be used as the file format
-//            ImageIOUtil.writeImage(bim, "./test" + "-" + (pageCounter++) + ".png", 300);
-//        }
-//        document.close();
         Writer output = new PrintWriter("./output/HTML/pdf.html", "utf-8");
         new PDFDomTree().writeText(page, output);
         output.close();
-        pdf.close();
         page.close();
+        pdf.close();
+        is.close();
+
 
     }
 
-    public static void  main(String[] args) throws IOException, ParserConfigurationException {
+    public static void toText(URL url) throws IOException {
+        InputStream is = url.openStream();
+        PDDocument document = PDDocument.load(is);
+        PDFTextStripper textStripper = new PDFTextStripper();
+        String str = textStripper.getText(document);
+        PrintWriter outTxt = new PrintWriter("./output/Texts/pdf.txt");
+        outTxt.println(str);
+        outTxt.close();
+        document.close();
+        is.close();
+    }
+
+        public static void  main(String[] args) throws IOException, ParserConfigurationException {
         File input = new File("input/input-sample-1.txt");
-        S3Client s3 = S3Client.builder().region(Region.US_EAST_1).build();
-        String bucket_name = "oo-dspsp-ass1";
-        String key_name = "inputTest";
-        ResponseInputStream<GetObjectResponse> test= s3.getObject(GetObjectRequest.builder().bucket(bucket_name).key(key_name).build());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(test));
-
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-
-        s3.deleteObject(DeleteObjectRequest.builder().bucket(bucket_name).key(key_name).build());
+//        S3Client s3 = S3Client.builder().region(Region.US_EAST_1).build();
+//        String bucket_name = "oo-dspsp-ass1";
+//        String key_name = "inputTest";
+//        ResponseInputStream<GetObjectResponse> test= s3.getObject(GetObjectRequest.builder().bucket(bucket_name).key(key_name).build());
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(test));
+//
+//        String line;
+//        while ((line = reader.readLine()) != null) {
+//            System.out.println(line);
+//        }
+//
+//        s3.deleteObject(DeleteObjectRequest.builder().bucket(bucket_name).key(key_name).build());
 //        try {
 //            S3Object o = s3.getObject(bucket_name, key_name);
 //            S3ObjectInputStream s3is = o.getObjectContent();
@@ -103,9 +105,10 @@ public class Test {
 //            System.exit(1);
 
 //        s3.putObject(PutObjectRequest.builder().bucket("oo-dspsp-ass1").key("inputTest").build(), RequestBody.fromFile(input));
-//        URL url = new URL("https://faculty.washington.edu/stuve/log_error.pdf");
-//        toHTML(url);
-//        toImage(url);
+        URL url = new URL("https://faculty.washington.edu/stuve/log_error.pdf");
+        toHTML(url);
+        toImage(url);
+        toText(url);
 
     }
 }
