9def1e8523d9e4a30e632d30ac5caaef6bc5daa1
Ohad Abramovitz <oh.ab28@gmail.com>
2021-11-18 14:57:54 +0200

    added tags for ec2, completed run check for the manager and moved ec2client to main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,7 @@ import software.amazon.awssdk.services.sqs.model.SendMessageBatchRequestEntry;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 //import java.util.Date;
 //import java.util.List;
+import javax.naming.Name;
 import java.io.File;
 //import java.util.Base64;
 import java.util.*;
@@ -47,40 +48,63 @@ public class Main {
 //        s3.putObject(PutObjectRequest.builder().bucket(bucket_name).key(key_name).build(), RequestBody.fromFile(input));
         String managerJarLoc =""; //TODO: get from s3
         String workerJarLoc =""; //TODO: get from s3
-        if (!isRunningEc2()){
-            runNewManager(managerJarLoc, workerJarLoc);
+        Ec2Client ec2 = Ec2Client.create();
+        if (!isRunningEc2(ec2)){
+            runNewManager(ec2,managerJarLoc, workerJarLoc);
         }
 
 
-        ec2Try();
-//        sqsTry();
+        //ec2Try();
+       // sqsTry();
     }
 
-    private static void runNewManager(String managerJar, String workerJar ) {
-        Ec2Client ec2 = Ec2Client.create();
+    private static void runNewManager(Ec2Client ec2,String managerJar, String workerJar ) {
+        System.out.println("Running new manager");
         String amiId = "ami-04ad2567c9e3d7893";
         String javaInstallation = "wget --no-check-certificate --no-cookies --header \"Cookie: oraclelicense=accept-securebackup-cookie\" http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.rpm\n"+
                 "sudo yum install -y jdk-8u141-linux-x64.rpm\n";
-
+        //sg-0a245fb00956df9ba security group ohad
+        //sg-0f83f8c78a44f97a6 security group ori
         RunInstancesRequest runRequest = RunInstancesRequest.builder()
                 .instanceType(InstanceType.T2_MICRO)
                 .imageId(amiId)
                 .maxCount(1)
                 .minCount(1)
                 .keyName("key1")
-                .securityGroupIds("sg-0f83f8c78a44f97a6")
+                .securityGroupIds("sg-0a245fb00956df9ba")
                 .userData(Base64.getEncoder().encodeToString(
                         ("#!/bin/bash\n"+
                                 javaInstallation+
                                 managerJar
                                 ).getBytes()))
                 .build();
+//        String reservation_id = ec2.describeInstances().reservations().get(0).instances().get(0).instanceId();
         RunInstancesResponse response = ec2.runInstances(runRequest);
+
+        String instanceId = response.instances().get(0).instanceId();
+
+        ec2.createTags(CreateTagsRequest.builder().resources(instanceId).tags(Tag.builder()
+                .key("Name").value("Manager").build()).build());
+
+
+
+//        List<Reservation> reservList = ec2.describeInstances().reservations();
+//        List<Instance> instanceList =  reservList.get(0).instances();
+        //RunInstancesResponse response = ec2.runInstances(runRequest);
         List<Instance> instances = response.instances();
         System.out.println(instances);
     }
 
-    private static boolean isRunningEc2() {
+    private static boolean isRunningEc2(Ec2Client ec2) {
+        List<Reservation> reservations = ec2.describeInstances().reservations();
+        for (Reservation reservation:reservations) {
+            Instance instance = reservation.instances().get(0);
+            if(!instance.tags().isEmpty() && instance.tags().get(0).value().equals("Manager"))
+                if(instance.state().nameAsString().equals("running")) {
+                    System.out.println("Manager is already running");
+                    return true;
+                }
+        }
         return false;
     }
 
@@ -148,7 +172,6 @@ public class Main {
         SendMessageRequest send_msg_request = SendMessageRequest.builder()
                 .queueUrl(queueUrl)
                 .messageBody("hello world")
-                .delaySeconds(5)
                 .build();
         sqs.sendMessage(send_msg_request);
 
@@ -164,7 +187,6 @@ public class Main {
                         ,
                         SendMessageBatchRequestEntry.builder()
                                 .messageBody("Hello from message 2 - Ori")
-                                .delaySeconds(10)
                                 .id("msg_2")
                                 .build())
                 .build();
@@ -176,18 +198,28 @@ public class Main {
                 .build();
         List<Message> messages = sqs.receiveMessage(receiveRequest).messages();
         System.out.println(messages.toString());
+        receiveRequest = ReceiveMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .build();
+         messages = sqs.receiveMessage(receiveRequest).messages();
+        System.out.println(messages.toString());
+        receiveRequest = ReceiveMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .build();
+        messages = sqs.receiveMessage(receiveRequest).messages();
+        System.out.println(messages.toString());
         for (Message m : messages) {
             System.out.println(m.body());
         }
 
 //         delete messages from the queue
-        for (Message m : messages) {
-            DeleteMessageRequest deleteRequest = DeleteMessageRequest.builder()
-                    .queueUrl(queueUrl)
-                    .receiptHandle(m.receiptHandle())
-                    .build();
-            sqs.deleteMessage(deleteRequest);
-        }
+//        for (Message m : messages) {
+//            DeleteMessageRequest deleteRequest = DeleteMessageRequest.builder()
+//                    .queueUrl(queueUrl)
+//                    .receiptHandle(m.receiptHandle())
+//                    .build();
+//            sqs.deleteMessage(deleteRequest);
+//        }
     }
 
 }
