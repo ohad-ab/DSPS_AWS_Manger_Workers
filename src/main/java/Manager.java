d8e717bf35dc5308c058ea0ee9f8d8d8456aa158
Ohad Abramovitz <oh.ab28@gmail.com>
2021-11-30 00:47:47 +0200

    Manager: added createOutput method and receiving messages from workers in Request.

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,34 +1,34 @@
 
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.*;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class Manager {
     private static boolean terminate = false;
     public static int activeWorker = 0;
     private LinkedBlockingQueue<InFile> queue;
     public static int workerCount = 0;
+    public static String bucket = "dsps-221";
+    public static String localManagerSQSurl = "https://sqs.us-east-1.amazonaws.com/150025664389/LOCAL-MANAGER";
+
 
     public static void main(String[] args) {
         System.out.println("Manager Started");
-        String localManagerSQSurl = "https://sqs.us-east-1.amazonaws.com/150025664389/LOCAL-MANAGER";
         waitForMessages(localManagerSQSurl);
 
 //        String requestsSqsUrl = "https://sqs.us-east-1.amazonaws.com/445821044214/requests_queue"; //Ori
@@ -152,6 +152,19 @@ public class Manager {
         List<Instance> instances = response.instances();
         System.out.println(instances);
     }
+    private static File createOutput(List<String>messages)
+    {
+        PrintWriter  outTxt = null;
+        try {
+          outTxt = new PrintWriter("./output.txt");
+          for(String message: messages)
+              outTxt.println(message);
+          outTxt.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new File("./output.txt");
+    }
 
 
 //        try {
@@ -177,16 +190,16 @@ public class Manager {
 
 
     static class Request implements Runnable {
-        private String mesaage;
+        private String localMessage;
 
-        public Request(String message) {
-            this.mesaage = message;
+        public Request(String localMessage) {
+            this.localMessage = localMessage;
         }
 
         @Override
         public void run() {
             try {
-                String[] splitMessage = mesaage.split("\t");
+                String[] splitMessage = localMessage.split("\t");
                 String[] splitURL = splitMessage[0].split("/");
                 int ratio = Integer.parseInt(splitMessage[1]);
                 S3Client s3 = S3Client.builder().region(Region.US_EAST_1).build();
@@ -198,18 +211,44 @@ public class Manager {
                     messagesToWorkers.add(reader.readLine());
                     messagecount++;
                 }
-                SqsClient workerSQS = SqsClient.builder().region(Region.US_EAST_1).build();
+                SqsClient sqs = SqsClient.builder().region(Region.US_EAST_1).build();
                         String requestsSqsUrl = "https://sqs.us-east-1.amazonaws.com/445821044214/requests_queue"; //Ori
                         String answersSqsUr = "https://sqs.us-east-1.amazonaws.com/445821044214/answers_queue"; //Ori
 
                 //        String requestsSqsUrl = ""; //Ohad
                 //        String answersSqsUr = ""; //Ohad
                 SendMessageRequest send_msg_request;
-                for (String messagesToWorker : messagesToWorkers)
-                    SendMessageRequest.builder().queueUrl(requestsSqsUrl).messageBody(messagesToWorker).build();
+                for (String messagesToWorker : messagesToWorkers) {
+                    send_msg_request=SendMessageRequest.builder().queueUrl(requestsSqsUrl).messageBody(messagesToWorker).build();
+                    sqs.sendMessage(send_msg_request);
+                }
                 int numOfWorkers = messagecount/ratio + (messagecount%ratio != 0?1:0);
                 for(int i = 0;i<numOfWorkers; i++)
                     runNewWorker(requestsSqsUrl, answersSqsUr);
+
+                ArrayList<String> messagesToManager = new ArrayList<>();
+
+                while (true)
+                {
+                    ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder().queueUrl(answersSqsUr).build();
+                    List<Message> messages = sqs.receiveMessage(receiveMessageRequest).messages();
+                    //System.out.println(messages.size());
+                    if(!messages.isEmpty())
+                        for(Message message:messages)
+                        {
+                            messagesToManager.add(message.body());
+                            System.out.println("message received!");
+                            DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder().queueUrl(answersSqsUr).receiptHandle(message.receiptHandle()).build();
+                            sqs.deleteMessage(deleteMessageRequest);
+                            messagecount--;
+                        }
+                    if(messagecount == 0)
+                        break;
+                }
+                File output = createOutput(messagesToManager);
+                s3.putObject(PutObjectRequest.builder().bucket(bucket).key("output").build(), RequestBody.fromFile(output));
+                send_msg_request=SendMessageRequest.builder().queueUrl(localManagerSQSurl).messageBody("s3://"+bucket+"/"+"output").build();
+                sqs.sendMessage(send_msg_request);
 
             } catch (Exception e) {
             }
